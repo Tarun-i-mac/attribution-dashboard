b68c4c9ce1e5887a688ae8d33c9434abce8fa1ee
Tarun-i-mac <tarunsuresh30@gmail.com>
2026-05-18 19:28:04 +0530

    Added user journry events

---
+++ dashboard/models/staging/stg_user_journey_events.sql
@@ -0,0 +1,25 @@
+SELECT
+  user_pseudo_id,
+  event_timestamp AS raw_event_timestamp,
+  CAST(TIMESTAMP_MICROS(event_timestamp) AS TIMESTAMP) AS event_ts,
+  CAST(TIMESTAMP_MICROS(event_timestamp) AS DATE) AS event_date,
+  event_name,
+  traffic_source.source AS source,
+  traffic_source.medium AS medium,
+  traffic_source.name AS campaign,
+
+  CASE
+    WHEN traffic_source.medium IS NULL THEN 'Direct'
+    WHEN traffic_source.medium = '(none)' THEN 'Direct'
+    WHEN traffic_source.medium = 'organic' THEN 'Organic Search'
+    WHEN traffic_source.medium = 'cpc' THEN 'Paid Search'
+    WHEN traffic_source.medium = 'referral' THEN 'Referral'
+    WHEN traffic_source.medium = '<Other>' THEN 'Other'
+    WHEN traffic_source.medium = '(data deleted)' THEN 'Unknown'
+    ELSE INITCAP(traffic_source.medium)
+  END AS channel,
+
+  ecommerce.purchase_revenue AS purchase_revenue
+
+FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
+WHERE _TABLE_SUFFIX BETWEEN '20210120' AND '20210131'
