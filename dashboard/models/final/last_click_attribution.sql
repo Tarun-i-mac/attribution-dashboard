7f053f20c884e88ba6e21d4abd5b6466cfe9b97a
Tarun-i-mac <tarunsuresh30@gmail.com>
2026-05-19 00:49:12 +0530

    Changed lookup and added test for intermediate

--- dashboard/models/final/last_click_attribution.sql
+++ dashboard/models/final/last_click_attribution.sql
@@ -15,7 +15,8 @@ WITH base AS (
   FROM `project-92d8b8b8-6c6a-463a-88d.labs.int_completed_purchases` p
   JOIN `project-92d8b8b8-6c6a-463a-88d.labs.int_customer_touchpoints` t
     ON p.user_pseudo_id = t.user_pseudo_id
-   AND t.event_ts <= p.event_ts
+    AND t.event_ts <= p.event_ts
+    AND t.event_ts >= TIMESTAMP_SUB(p.event_ts, INTERVAL 7 DAY)
 )
 
 SELECT *
